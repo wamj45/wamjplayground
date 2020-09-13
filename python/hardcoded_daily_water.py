c88fdb60b3b5cf4321c91d91939916e00b8da33c
Wilbert Martinez <wamj45@gmail.com>
2020-09-13 11:27:11 -0500

    working on the hardcording script

--- python/hardcoded_daily_water.py
+++ python/hardcoded_daily_water.py
@@ -1,4 +1,24 @@
-gender = input('Are you male or female: [m/f]')
 
-if gender == 'm':
-    print('you have entered male')
+class DailyWaterIntake:
+
+    def __init__(self, water):
+        self.gender = gender
+        self.water = None
+
+    def gender(self):
+        gender = input('Are you male or female: [m/f]')
+
+
+    def neccessary_water(self, water):
+        try:
+            self.gender
+
+            if self.gender == 'm':
+                print('you have entered male')
+                self.water = 124
+                print(self.water)
+
+            if self.gender == 'f':
+                print('you have etered female')
+        except Exception as arg:
+            print('Error due to [{}]'.format(str(arg)))
