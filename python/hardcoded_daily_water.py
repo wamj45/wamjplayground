e24418c98868cfb3af83cb91d53a95eec0a95eb8
Wilbert Martinez <wamj45@gmail.com>
2020-09-17 13:52:13 -0500

    update to the hardcoded_water script

--- python/hardcoded_daily_water.py
+++ python/hardcoded_daily_water.py
@@ -1,42 +1,29 @@
 
-class DailyWaterIntake:
-
-    def __init__(self, gender):
-        self.gender = gender
-
-    #
-    # def gender(self):
-    #     gender = input('Are you male or female: [m/f]')
-
-    def neccessary_water(self):
-        try:
-            self.gender
-            if self.gender.lower() == 'male':
-                water = 124
-
-            if self.gender.lower() == 'female':
-                water = 92
-
-        except Exception as arg:
-            print('Error due to [{}]'.format(str(arg)))
-            return False
-
-        return 'You have entered {}. Therefore your daily water intake is {}oz.'.format(
-            self.gender, water)
-
-male = DailyWaterIntake('Female')
-print(male.neccessary_water())
-
+# class DailyWaterIntake:
+#will add class capability after making the function version work
 #
-# def main():
-#
-#
-#     waterintake = DailyWaterIntake()
-#     if waterintake.neccessary_water() is False:
-#         print('Unabale to calculatedaily water intake')
-#         sys.exit(1)
-#
-#
-#
-# if __name__ == '__main__':
-#     main()
+# def __init__(self, gender):
+#     self.gender = gender
+
+def user_gender(self):
+    return self.gender
+
+def neccessary_water(self):
+    try:
+        self.gender
+        if self.gender.lower() == 'male':
+            water = 124
+
+        if self.gender.lower() == 'female':
+            water = 92
+
+    except Exception as arg:
+        print('Error due to [{}]'.format(str(arg)))
+        return False
+
+    return 'You have entered {}. Therefore your daily water intake is {}oz.'.format(
+        self.gender, water)
+
+entered_gender = input('Please enter your gender[m/f]: ')
+
+check = user_gender(entered_gender)
--- python/learning_scripts.py
+++ python/learning_scripts.py
@@ -1,9 +1,26 @@
-def make_shirt(size, message):
-    print('\nThis is for shirt size {}.'.format(size))
-    print('This is the message on the shirt "{}".'.format(message))
+# def make_shirt(message, size):
+    # print('\nThis is for shirt size {}.'.format(size))
+    # print('This is the message on the shirt "{}".'.format(message))
+#
+# shirt_size = input('Please enter a shirt size [S,M,L,XL]: ')
+# shirt_message = input('What do you want the shirt to say?: ')
+#
+# make_shirt(message=shirt_message, size=shirt_size)
 
-# make_shirt('L', 'Red Sox')
-shirt_size = input('Please enter a shirt size [S,M,L,XL]: ')
-shirt_message = input('What do you want the shirt to say?: ')
 
-make_shirt(size=shirt_size, message=shirt_message)
+def city_country(city, country):
+    return '{}, {}.'.format(city, country)
+
+# chosen_city = input('Please enter the city: ').title()
+# chosen_country = input('Please enter the country: ').title()
+#
+# output = describe_city(city=chosen_city, country=chosen_country)
+# print(output)
+
+check = city_country('Santiago', "Chile")
+dr_check = city_country('Santo Domingo', "Republica Dominicana")
+rome = city_country('Rome', 'Italy')
+
+print(check)
+print(dr_check)
+print(rome)
