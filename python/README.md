5d2c44644b1685fa4156def8faa4250e84bf906b
Wilbert Martinez <wamj45@gmail.com>
2020-09-19 17:16:05 -0500

    Created the first version of a README

---
+++ python/README.md
@@ -0,0 +1,6 @@
+# README
+
+### Welcome to Wilbert A Martinez Jr's GitHub
+
+What you will find here is my personal programming portfolio.
+I have organized my code by language for simplicity. 
--- python/hardcoded_daily_water.py
+++ python/hardcoded_daily_water.py
@@ -5,35 +5,47 @@ class DailyWaterIntake:
 
     def __init__(self, gender):
         self.gender = gender
+        self.water_needed = ''
 
-    def user_gender(self):
-        return gender
+    def water_bottles_drunk(self):
+
+        water_bottle = 16.9
+        drank = int(input('Please enter how many water bottles you have drunken from: '))
+        if drank == 0:
+            print('Ok cool no water bottles for you :)')
+        else:
+            amt_water_from_bottles = drank * water_bottle
+            print('That equates to {}'.format(amt_water_from_bottles))
+
+
+        return True
 
     def neccessary_water(self):
 
-        water = ''
+        if self.water_bottles_drunk() is False:
+            print('So you did not drink from a water bottle today. Cool.')
+            return False
+
         try:
             self.gender
             if self.gender.lower() == 'male':
-                water = 124
-
-
+                self.water_needed = 124
             if self.gender.lower() == 'female':
-                water = 92
-
+                self.water_needed = 92
         except Exception as arg:
             print('Error due to [{}]'.format(str(arg)))
             return False
-
+        #Need to edit this print statement. the you have enterd part is kinda
+        #weird as the program expands
         print('You have entered {}. Therefore your daily water intake is {} oz.'.format(
-            self.gender, water))
+            self.gender, self.water_needed))
 
         return True
 
 
 def main():
 
-    user = input('Please enter your gender[m/f]: ')
+    user = input('Please enter the gender you identify most with(male/female): ')
     water_test = DailyWaterIntake(gender=user)
 
     if water_test.neccessary_water() is False:
