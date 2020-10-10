8f3936c0146ee848ec806b2703ba6f882eea0685
Wilbert <wamj45@gmail.com>
2020-10-10 16:31:21 -0500

    Almost done with the water calculator

--- python/daily_water_calculator.py
+++ python/daily_water_calculator.py
@@ -7,76 +7,87 @@ class DailyWaterIntake:
 
     def __init__(self, gender):
         self.gender = gender
-        self.daily_water = None
-        self.water_drunk = 0
-        self.total_water_drunk = 0
+        self.necessary_water = None
+        self.water_drunk = None
+        self.total_water_drunk = None
+        self.bottle_water = None
 
     def amt_water_drunk(self):
-        #need to fadd a False check
+
+        if self.water_bottles_drunk() is False:
+            print('Unable to calculate how many water bottles were drunk')
+            return False
+
         water_drank = int(input('Please enter the amount of water you have dranken today: '))
 
         if water_drank == 0:
+            self.water_drunk = self.bottle_water
             print('Dang no water?!')
 
         else:
-            self.water_drunk = self.water_drunk + water_drank
+            self.water_drunk = self.bottle_water + water_drank
             # Call the self.total_water_drunk here and create its amt of water
 
         return True
 
     def water_bottles_drunk(self):
-        #need to add a False statement in theis method
+
         water_bottle = 16.9
         bottles_drank = int(input('Please enter how many water bottles you have drunken from: '))
         if bottles_drank == 0:
             print('Ok cool no water bottles for you :)')
+            self.bottle_water = 0
         else:
             amt_water_from_bottles = bottles_drank * water_bottle
             print('That equates to {:.2f}oz'.format(amt_water_from_bottles))
-            # Water the heck is this variable 'self.water'?
-            # TODO:
-            # create a new list and function which contains the total amt of
-            # water drank in hte day
-            self.water = amt_water_from_bottles
+
+            self.bottle_water = amt_water_from_bottles
 
         return True
 
     def neccessary_water(self):
 
-        if self.water_bottles_drunk() is False:
-            print('So you did not drink from a water bottle today. Cool.')
-            return False
-
         try:
             self.gender
 
             if self.gender.lower() == 'male':
-                self.daily_water = 124
+                self.necessary_water = 124
 
             if self.gender.lower() == 'female':
-                self.daily_water = 92
+                self.necessary_water = 92
         except Exception as arg:
             print('Error due to [{}]'.format(str(arg)))
             return False
-        #Need to edit this print statement. the you have enterd part is kinda
-        #weird as the program expands
-        # print('You have entered {}. Therefore your daily water intake is {} oz.'.format(
-        #     self.gender, self.daily_water))
 
-        # This check does not make sense how can it be False
+        return True
+
+    def total_water(self):
+        # This should be called first and cause the rest of the program to run
         if self.amt_water_drunk() is False:
-            print("So you have not drinken any water today, that's OK fam!")
+            print('Unable to initialize program')
             return False
 
-        else:
-            # Think about what you want to put here. We can have it just return
-            # True, nonething at all, ie remove the else statement, or have it print
-            # put something
+        if self.neccessary_water() is False:
+            print('Unable to calculate your necessary daily water intake')
+            return False
+
+        self.total_water_drunk = self.water_drunk - self.necessary_water
+
+        if self.total_water_drunk < 0:
+            # This means you still need to drink more water
+            # Convert to a positive number for display
+            water_needed = self.total_water_drunk * (-1)
+            print('You still should drink {:.2f}oz of water'.format(water_needed))
 
 
-            return True
+        elif self.total_water_drunk >= 0:
+            # this means you have met yur quota for water intake
+            print('You drank enough water for today!')
+            print('You are {}oz over your daily needs'.format(self.total_water_drunk))
 
 
+
+        return True
 
 
 def main():
@@ -84,7 +95,7 @@ def main():
     user = input('Please enter the gender you identify most with(male/female): ')
     water_test = DailyWaterIntake(gender=user)
 
-    if water_test.neccessary_water() is False:
+    if water_test.total_water() is False:
         print('Unable to calculate the neccesart water intake for you')
         sys.exit(1)
 
