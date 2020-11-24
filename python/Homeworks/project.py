29348981f57c5af13091e2e918e023911b31cdae
Wilbert <wamj45@gmail.com>
2020-11-24 00:11:51 -0600

    Progress on the project

--- python/Homeworks/project.py
+++ python/Homeworks/project.py
@@ -18,10 +18,14 @@ import os
 import json
 import sys
 import math
+import fnmatch
+import re
 
 # Python Imaging Library(PIL) used to manipulate images in Python
 from PIL import Image
+from PIL import ImageStat
 
+FILENAME_REGEX = '[0-9,A-Z,_-]+'
 
 class ImageEvaluator:
 
@@ -30,6 +34,14 @@ class ImageEvaluator:
         self.path = path
         self.image_list = []
         self.data = {}
+    # Changes the dir to the path containing images
+    def get_correct_dir(self):
+        current_dir = os.getcwd()
+        if current_dir == self.path:
+            return self.path
+        else:
+            correct_dir = os.chdir(self.path)
+            return correct_dir
 
     def load_images(self):
     # Loads images into the image list. If no images found, exits program with error message
@@ -46,23 +58,24 @@ class ImageEvaluator:
                 return False
 
             for file_type in files:
-                print('test')
-                if jpg_type in file_type:
+                # print('test')
+                if fnmatch.fnmatch(file_type, jpg_type):
                     self.image_list.append(file_type)
-                if JPG_type in file_type:
+                elif fnmatch.fnmatch(file_type, JPG_type):
                     self.image_list.append(file_type)
-                if jpeg_type in file_type:
+                elif fnmatch.fnmatch(file_type, jpeg_type):
                     self.image_list.append(file_type)
-                if JPEG_type in file_type:
+                elif fnmatch.fnmatch(file_type, JPEG_type):
                     self.image_list.append(file_type)
-                if png_type in file_type:
+                elif fnmatch.fnmatch(file_type, png_type):
                     self.image_list.append(file_type)
-                if PNG_type in file_type:
+                elif fnmatch.fnmatch(file_type, PNG_type):
                     self.image_list.append(file_type)
                 else:
                     print('Error - Unable to locate any image files in [{}]. Please try a diffent file path.'
                         .format(self.path))
                     return False
+
         return True
 
     def brightness_calculation(self):
@@ -71,20 +84,40 @@ class ImageEvaluator:
             print('Error - Unable to begin brightess calculation')
             return False
 
+        dir = self.get_correct_dir()
+
         for image in self.image_list:
-            #Converts image to greyscale
-            img = image.open(image).convert('L')
+            # Convert image to greyscale (black and white)
+            img = Image.open(image).convert('L')
             stat = ImageStat.Stat(img)
-            # R,G,B average values
-            r = stat.mean
-            g = stat.mean
-            b = stat.mean
-            brightness = math.sqrt(0.241*(r**2) + 0.691*(g**2) + 0.068*(b**2))
+            # average brightness of all pixels, returns as a list of one element
+            brightness = stat.mean[0]
+            print('[{}] has a brightness of: [{}]'.format(image, brightness))
+
+            # Todo:
+            # Add to dictionary the brightess level of each image
+            # self.data[image] = brightness
 
         return True
 
     def luminance_calculation(self):
 
+        dir = self.get_correct_dir()
+
+        for image in self.image_list:
+            # print(image)
+            img = Image.open(image)
+            stat = ImageStat.Stat(img)
+            # R,G,B average values
+            rgb_values = stat.mean
+            r = stat.mean[0]
+            g = stat.mean[1]
+            b = stat.mean[2]
+
+            # Todo:
+            # Call this method
+            # Add a check
+            # Add to the dictionary the calues of relative luminance
 
         return True
 
@@ -97,8 +130,8 @@ def main():
     # May have to add a click and drag into terminal
     # Asks user to enter dir containing images
     path_message = 'Please enter the directory/folder containing the images: '
-    user_path = input(path_message)
-
+    # user_path = input(path_message)
+    user_path = '/home/wamj/Pictures/Pics/'
     # Calls the ImageEvaluator class and exits if cannot run
     image_evaluater = ImageEvaluator(user_path)
     if image_evaluater.brightness_calculation() is False:
