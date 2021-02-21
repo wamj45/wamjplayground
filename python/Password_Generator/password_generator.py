2dda0a90c75add0cce9a5034ea3756120b08cbc7
Wilbert <wamj45@gmail.com>
2021-02-21 08:47:28 -0600

    PasswordGenerator.py structure is set up

--- python/Password_Generator/password_generator.py
+++ python/Password_Generator/password_generator.py
@@ -9,16 +9,36 @@ import uuid
 
 class PasswordManager():
 
-    def __init__(self, site):
+    def __init__(self, site, note):
         self.site = site
+        self.note = note
+        self.password_file = "PasswordManager.txt"
 
+    def create_password(self):
+        return password
 
+    def create_txt_file(self, file):
+        create_file = open(file, "w+")
+        create_file.close()
+        return create_txt_file
 
+    def write_txt_file(self):
+        if self.open_txt_file(self.password_file) is False:
+            self.create_txt_file(self.password_file)
+        return True
 
+    def open_txt_file(self, file):
+        f = open(file, "r")
+        return f
 
 def main():
-    site = input("\nPlease enter the site/application we are generating a password for: ")
-    print(f"This is the {site} we are working with")
+    site = input("Please enter the site/application we are generating a password for: ")
+    note = input("\nPlease enter a quick note about this password: ")
+
+    passwordmanager = PasswordManager(site, note)
+    if passwordmanager.write_txt_file() is False:
+        print("Error - Unable to open/create the text file for password")
+        sys.exit(255)
 
 if __name__ == "__main__":
     main()
