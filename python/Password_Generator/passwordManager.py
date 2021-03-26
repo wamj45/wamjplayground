8d559a6a82f178d656961ab13c50b1a96e5ba1e8
Wilbert <wamj45@gmail.com>
2021-03-25 23:49:24 -0500

    Making progress on the PasswordManager script

--- python/Password_Generator/passwordManager.py
+++ python/Password_Generator/passwordManager.py
@@ -26,16 +26,25 @@ class PasswordManager():
         try:
             self.open_txt_file(self.password_file)
         except Exception as arg:
-            print(f"Error - No {self.password_file} found" + str(arg))
+            print("Error - " + str(arg))
             print(f"Creating the Password Manager file in this dir...")
 
             self.create_txt_file(self.password_file)
-        
+
+            # Write the site, note and the password into the file
+            # Close the file
+            # Use a dictionary
         return True
 
     def open_txt_file(self, file):
         f = open(file, "r")
         return f
+
+    def generate_password(self):
+        generate = str(uuid.uuid4())
+        password = generate.replace("-", "")
+        return password
+
 
 def main():
     site = input("Please enter the site/application we are generating a password for: ")
---
+++ python/Reminders/reminder.py
@@ -0,0 +1,2 @@
+# Should remind me of all the things I need to do
+# Should remind me in some way of the things I need to do
