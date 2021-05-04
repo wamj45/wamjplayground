d47470d4c56723f41d590fd659e6b0c0a34efa6f
Wilbert <wamj45@gmail.com>
2021-05-03 23:12:18 -0500

    working on the filtering the correct files

--- python/STL_CleanUp/locate_files.py
+++ python/STL_CleanUp/locate_files.py
@@ -14,10 +14,15 @@ class FileFinder():
     def locate_zipfiles(self):
         pattern = "*.zip"
         self.zip_files = self.match_files(pattern)
+        wanted_files = []
         # at this point we have a list of ALL zip files in the dir
-        for file in self.zip_files:
-
-        return self.zip_files
+        # we need to filter out the the ones we don't want
+        for zip_file in self.zip_files:
+            val = input(f"Is [{zip_file}] the file we are looking for? (y/n)\n")
+            if val == "y":
+                wanted_files.append(zip_file)
+        print(wanted_files)
+        return wanted_files
 
     def locate_stl_files(self):
         pattern = "*.stl"
@@ -42,7 +47,7 @@ def main():
     if files_manager.locate_zipfiles() is False:
         print("Error - Unable to locate downloads dir")
         sys.exit(0)
-    print(files_manager.locate_zipfiles())
+    # print(files_manager.locate_zipfiles())
 
 if __name__ == "__main__":
     main()
