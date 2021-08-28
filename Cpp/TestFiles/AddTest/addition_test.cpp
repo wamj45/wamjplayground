3acd7a04501cc2a4bc45f4b76d572031333be7b8
Wilbert <wamj45@gmail.com>
2021-08-28 16:38:47 -0500

    Learning C++

--- Cpp/TestFiles/AddTest/addition_test.cpp
+++ Cpp/TestFiles/AddTest/addition_test.cpp
@@ -2,7 +2,7 @@
 int main()
 {
   std::cout << "Enter two numbers:" << std::endl;
-  double v1 = 0, v2 = 0;
+  double v1 = 0, v2 = 0; //Made into a double to handle addition of doubles
   std::cin >> v1 >> v2;
   std::cout << "The sum of " << v1 << " and " << v2
             << " is " << v1+v2 << std::endl;
