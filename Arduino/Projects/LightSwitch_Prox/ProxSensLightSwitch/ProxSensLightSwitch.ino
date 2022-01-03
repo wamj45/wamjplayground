288d0ea65bfee2fcc088731a9e9f7a17b827e2fc
Wilbert Martinez <wamj45@Wilberts-MacBook-Pro-2.local>
2022-01-02 23:05:28 -0600

    Init Arduino Dir

---
+++ Arduino/Projects/LightSwitch_Prox/ProxSensLightSwitch/ProxSensLightSwitch.ino
@@ -0,0 +1,20 @@
+#define RELAY_PIN 5
+#define PROX_SENS_PIN 4
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Starting...");
+
+  pinMode(RELAY_PIN, OUTPUT);
+  //Test which works best input or input_pullup
+  pinMode(PROX_SENS_PIN, INPUT);
+  //pinMode(PROX_SENS_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+  if (digitalRead(PROX_SENS_PIN) == LOW) {
+    Serial.println("Door is open.\nTurning Lights on!");
+    digitalWrite(RELAY_PIN, HIGH);
+  }
+  delay(500);
+}
