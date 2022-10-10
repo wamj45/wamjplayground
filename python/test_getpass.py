b6277e27dcdf7e9968a8383379d924aedc723f3b
Wilbert A Martinez Jr <wamj45@gmail.com>
2022-10-09 22:52:22 -0500

    Played with test_getpass.py

--- python/test_getpass.py
+++ python/test_getpass.py
@@ -1,10 +1,15 @@
-# A simple Python program to demonstrate
-# getpass.getpass() to read password
-import getpass
+'''
+A simple Python program to demonstrate
+#getpass.getpass() to read password
+'''
+
+import getpass, os
 
 try:
 	password = getpass.getpass('Enter password: ')
 except Exception as error:
-	print('ERROR', error)
+	print(f"Failed to set password:\n{str(error)}")
 else:
 	print(f'You entered: {password}')
+	os._exit(0)
+
